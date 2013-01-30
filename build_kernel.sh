f85b91dd5a3b7e5b4f2dfc813b9915bc86e2c6fd
k0nane <k0@k0nane.info>
2013-01-29 21:53:02 -0600

    Correct defconfig name, I'm an idiot

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,7 @@ CWD=$PWD
 
 KERNEL_DIR=$CWD/Kernel
 BUILDS_DIR=~/android/builds
-DEFCONFIG=trebon_usa_k0_defconfig
+DEFCONFIG=trebon_usa_03_defconfig
 CROSS_COMPILER=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi-
 THREADS=4
 GENERATE_ZIP=y
